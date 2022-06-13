3c1879c1d61005b593b510ed707fa49e7fd585aa
HanYuanDao <hanzhe.jason@gmail.com>
2022-06-13 13:38:33 +0800

    revert

--- rqdata/init.py
+++
@@ -1,4 +0,0 @@
-import rqdatac as rq
-from rqdatac import *
-
-rq.init(13971447255, 'hzlianhd520')
--- test.py
+++
@@ -1,7 +0,0 @@
-import time
-import datetime
-
-t = datetime.datetime.now()
-
-mills = int(time.mktime(t.timetuple())) * 1000 + int(t.microsecond / 1000)
-print(mills)
