e8898d84ac7d5d405c1a33466e5547b430811a05
HanYuanDao <hanzhe.jason@gmail.com>
2023-04-25 10:58:13 +0800

    添加debug日志

--- vnpy_ctastrategy/strategies/the_boll_tactic_of_nd.py
+++ vnpy_ctastrategy/strategies/the_boll_tactic_of_nd.py
@@ -136,9 +136,7 @@ class TheBollTacticOfND(CtaTemplate):
                                memo=self.strategy_trade_memo)
 
     def on_tick(self, tick: TickData):
-        self.output("开始on_tick")
         self.bg.update_tick(tick)
-        self.output("结束bg.update_tick")
         self.tick_now = tick
 
         # if self.tick_now.datetime.month == 2 \
@@ -200,7 +198,6 @@ class TheBollTacticOfND(CtaTemplate):
                                        abs(self.pos),
                                        net=True,
                                        memo=self.strategy_trade_memo)
-        self.output("结束on_tick")
 
     def on_bar(self, bar: BarData):
         self.bg.update_bar(bar)
